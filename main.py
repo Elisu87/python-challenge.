e83bf59c119a64bf1ddb19a493095079c2fb5d14
Elisu87 <54041709+Elisu87@users.noreply.github.com>
2019-10-26 20:32:43 -0500

    PyPoll_Exercise

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 # access csv budget_data csv
 import os
 import csv
-csvpath = os.path.join('python-challenge.','budget_data.csv')
+from collection import defaultdict
+csvpath = os.path.join('python-challenge.', 'election_data.csv')
+
 with open(csvpath,'r', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-    # Create lists to store date input & profit and lost input
-    months = []
-    amount = []
+    # Create lists to input the votes and candidates 
+    
+    total_votes = 0
+    candidates = set()
+    votes_num= []
+    ballots =[]
+    
+    #Iterate to search for number of vote and candidates with votes 
     for row in csvreader:
-        months.append(row[0])
-        amount.append(int(row[1]))
-# Print number of months within the database and amount over entire period
-print(len(months))
-print(len(amount))
-#Calculate total months and total value 
-print(f'Total Months:((len(amount))')   
-total = sum(amount)
-print(f'Total:[total]')
-# Calculating the avarage in changes 
-changes = []
-for index in range(len(amount)-1):
-    changes.append(amount[index+1]-amount[index])
-    print(sum(changes)/len(changes))
-    avg_changes = sum(changes)/len(changes)
-    print(f'Average Change:$[avg_changes]')
-#Calculating Greatest increase in profit and Greatest decrease
-    print(f'Greatest Increase in Profits: (max(changes))')
-    print(f'Greatest Decrease in Profits: ((max(changes))')
-# Export text file with results
-with open(f"{PyBank}/analysis.txt","w") as f:
-    f.write(f"Finanancial Analysis\n")
-    f.write(f"--------------------------\n")
-    f.write(f"Total Months:{len(amount)})\n")
-    f.write(f"Total:{total}\n")
-    f.write(f"Average Change:${avg_changes}\n")
-    f.write(f"Greatest Increase in Profits:{max(changes)})\n")
-    f.write(f"Greatest Decrease in Profits: {max(changes)})\n")
+        total_votes+= 1
+        candidates.add(row[2])
+        votes_num.append()
+        if ballot.get(int(row[2]))is None:
+            ballots(int(row[2])) = 0
+        else:
+            ballots(row[2])+= 1
+print(total_votes)
+print(candidates)
+print(ballots)
+#Printing percent per candidate
+for candidate in candidates:
+    candidate_vote = ballots.get(candidate)
+    percent = f"{(candidate_votes / total_votes*100)}"
+    print(f'{candidate} {percent}% {ballots[candidate]}')
+#Printing winner
+print(f"Winner:{max(ballot2, key=ballots.get)}")
+#Export as a txt file 
+with open(f"{dir_path}/results.txt","w") as f:
+f.write(f"Election Results\n")
+f.write(f"--------------------------\n")
+f.write("Total Votes:{(total_votes}\n")
+    for candidate in candidates:
+    candidate_vote = ballots.get(candidate)
+    percent = (f"{ballots.get(candidate/total_votes*100:)})
+    print(f'{candidate}%{percent}'%{ballots[candidates]})')
+f.write(f"Winner: {max(ballots, key=ballots.get)}\n")
+
